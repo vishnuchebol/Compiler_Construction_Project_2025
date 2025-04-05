abdc7f3216f6bd1d2ffec22a3412e3bd7748f4d3
Vishnu <vishnu.chebolu@gmail.com>
2025-04-05 12:31:18 +0530

    small commit

--- driver.h
+++ driver.h
@@ -2,7 +2,7 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
-#include<stdio.h>
+#include <stdio.h>
 
 #define NUM_OF_TERMINALS 61
 #define NUM_OF_NONTERMINALS 53
@@ -19,13 +19,13 @@
 #define MAX_ERROR_STRING_LENGTH 500
 #define MAX_ERROR_NUM 500
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 #define LEXICAL_ERROR 0
 #define SYNTACTIC_ERROR 1
@@ -37,7 +37,11 @@
 char terminal_string[NUM_OF_TERMINALS][MAX_SYMBOL_LENGTH];
 char non_terminal_string[NUM_OF_NONTERMINALS][MAX_SYMBOL_LENGTH];
 
-typedef enum {ast, pt} tree_type;
+typedef enum
+{
+    ast,
+    pt
+} tree_type;
 
 char parse_tree_file[MAX_FILE_NAME_LEN];
 char assembly_file[MAX_FILE_NAME_LEN];
